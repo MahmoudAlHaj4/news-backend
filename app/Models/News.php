252c96c61f9569a0fa1bfce94dbd4098aaeb35bb
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2025-08-03 00:10:49 +0300

    push controller

---
+++ app/Models/News.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'link',
+        'date'
+    ];
+
+    protected $casts = [
+        'date' => 'date'
+    ];
+
+
+    public function scopeLatest($query)
+    {
+        return $query->orderBy('date', 'desc');
+    }
+}
